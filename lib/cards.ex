5290c075cef3f4d734b4d1a8071adecb4f868040
s-loar <sjl7909@gmail.com>
2022-11-04 16:34:21 -0400

    Add more method documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -15,10 +15,16 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Returns a shuffled a deck.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Returns a boolean of whether a card exists in a deck.
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -39,11 +45,18 @@ defmodule Cards do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Saves a deck to the `filename` parameter.
+    By default, if puts it at the root of the project.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Loads and returns a deck from the `filename` parameter.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
@@ -51,6 +64,9 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Creates a deck, shuffles that deck, and deals a hand of `hand_size`.
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
